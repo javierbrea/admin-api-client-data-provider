02fdd662880dcda161c73b0c1d8ca1155cbe18ed
javierbrea <javier.brea@gmail.com>
2022-03-28 13:13:20 +0200

    chore: migrate to monorepo

--- test-e2e/browser/react-app/src/modules/about/AboutController.js
+++
@@ -1,8 +0,0 @@
-import { withData } from "@data-provider/react";
-import { about } from "@mocks-server/admin-api-client-data-provider";
-
-import AboutView from "./AboutView";
-
-const AboutController = withData(about, "about")(AboutView);
-
-export default AboutController;
--- test-e2e/browser/react-app/src/modules/settings/SettingsController.js
+++
@@ -1,8 +0,0 @@
-import { withData } from "@data-provider/react";
-import { settings } from "@mocks-server/admin-api-client-data-provider";
-
-import SettingsView from "./SettingsView";
-
-const SettingsController = withData(settings, "settings")(SettingsView);
-
-export default SettingsController;
